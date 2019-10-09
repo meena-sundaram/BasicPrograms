1d5c82690e4ea5caf42ca6d5ce23bdead0c5b835
Admin <Admin@DESKTOP-0EIURMD>
2019-10-09 22:02:03 +0530

    first commit

---
+++ Programs/src/OopsConcepts/Encap.java
@@ -0,0 +1,15 @@
+package OopsConcepts;
+
+public class Encap {
+	private int a=10;
+
+	public int getEncap() {
+		return a;
+	}
+
+	public void setEncap(int a) {
+		this.a = a;
+	}
+
+}
+
---
+++ Programs/src/OopsConcepts/EncapMain.java
@@ -0,0 +1,12 @@
+package OopsConcepts;
+
+public class EncapMain {
+
+	public static void main(String[] args) {
+		Encap a=new Encap();
+		System.out.println(a.getEncap());
+		a.setEncap(1000);
+		System.out.println(a.getEncap());
+	}
+}
+
